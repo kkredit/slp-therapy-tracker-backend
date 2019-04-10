573893f76bb8187ef3707f5aaa03e21f7ec36b4d
Kevin Kredit <k.kredit.us@ieee.org>
2019-04-09 22:49:23 -0400

    Implement Attempt tests and validations

--- app/models/attempt.rb
+++ app/models/attempt.rb
@@ -1,3 +1,12 @@
 class Attempt < ApplicationRecord
+  enum status: [:correct, :incorrect, :correct_with_cueing]
+
+  validates :number, presence: true, uniqueness: { scope: :goal_id },
+                     numericality: { only_integer: true,
+                                     greater_than_or_equal_to: 0,
+                                     less_than_or_equal_to: 100 }
+  validates :status, presence: true
+  validates :goal_id, presence: true
+
   belongs_to :goal
 end
--- test/models/attempt_test.rb
+++ test/models/attempt_test.rb
@@ -1,7 +1,69 @@
 require 'test_helper'
 
 class AttemptTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    # associate a attempt first
+    @goal = Goal.first
+
+    @attempt = Attempt.create(number: 0, goal_id: @goal.id, status: :correct)
+  end
+
+  test "'attempt' must be valid" do
+    assert @attempt.valid?
+  end
+
+  test "number must be present" do
+    @attempt.number = ""
+    assert_not @attempt.valid?
+  end
+
+  test "number must be an integer" do
+    @attempt.number = 0.5
+    assert_not @attempt.valid?
+  end
+
+  test "number must be unique per goal id" do
+    new_attempt = Attempt.create(number: @attempt.number, goal_id: @attempt.goal_id, status: :correct)
+    assert_not new_attempt.valid?
+    new_attempt = Attempt.create(number: @attempt.number, goal_id: Goal.last.id, status: :correct)
+    assert new_attempt.valid?
+  end
+
+  test "number must be great than or equal to 0" do
+    @attempt.number = -1
+    assert_not @attempt.valid?
+    @attempt.number = 0
+    assert @attempt.valid?
+  end
+
+  test "number must be less than or equal to 100" do
+    @attempt.number = 100
+    assert @attempt.valid?
+    @attempt.number = 101
+    assert_not @attempt.valid?
+  end
+
+  test "status must not be invalid" do
+    invalid_statuses = [-10, -1, 4, 10]
+    invalid_statuses.each do |is|
+      begin
+        @attempt.status = is
+        assert false, "#{is} should be invalid"
+      rescue
+        assert true
+      end
+    end
+  end
+
+  test "status must be valid" do
+    valid_statuses = [:correct, :incorrect, :correct_with_cueing]
+    valid_statuses.each do |is|
+      begin
+        @attempt.status = is
+        assert @attempt.valid?
+      rescue
+        assert false,  "#{is} should be invalid"
+      end
+    end
+  end
 end
