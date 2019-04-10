08cb44619dcecfa6797791230886d64724a188a9
Kevin Kredit <k.kredit.us@ieee.org>
2019-04-09 22:18:16 -0400

    Implement Student tests and validation

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,9 @@
 class Student < ApplicationRecord
+  validates :number, presence: true, uniqueness: { scope: :session_id },
+                     numericality: { only_integer: true,
+                                     greater_than_or_equal_to: 0,
+                                     less_than_or_equal_to: 3 }
+  validates :session_id, presence: true
+
   belongs_to :session
 end
--- test/models/student_test.rb
+++ test/models/student_test.rb
@@ -1,7 +1,45 @@
 require 'test_helper'
 
 class StudentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    # associate a session first
+    @session = Session.first
+
+    @student = Student.create(number: 0, session_id: @session.id)
+  end
+
+  test "'student' must be valid" do
+    assert @student.valid?
+  end
+
+  test "number must be present" do
+    @student.number = ""
+    assert_not @student.valid?
+  end
+
+  test "number must be an integer" do
+    @student.number = 0.5
+    assert_not @student.valid?
+  end
+
+  test "number must be unique per session id" do
+    new_student = Student.create(number: @student.number, session_id: @student.session_id)
+    assert_not new_student.valid?
+    new_student = Student.create(number: @student.number, session_id: Session.last.id)
+    assert new_student.valid?
+  end
+
+  test "number must be great than or equal to 0" do
+    @student.number = -1
+    assert_not @student.valid?
+    @student.number = 0
+    assert @student.valid?
+  end
+
+  test "number must be less than or equal to 3" do
+    @student.number = 3
+    assert @student.valid?
+    @student.number = 4
+    assert_not @student.valid?
+  end
 end
