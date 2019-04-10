b32796c451d105eb8df69418ea7bf17d78aec79c
Kevin Kredit <k.kredit.us@ieee.org>
2019-04-09 22:30:00 -0400

    Implement Goal tests and validations

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,3 +1,9 @@
 class Goal < ApplicationRecord
+  validates :number, presence: true, uniqueness: { scope: :student_id },
+                     numericality: { only_integer: true,
+                                     greater_than_or_equal_to: 0,
+                                     less_than_or_equal_to: 3 }
+  validates :student_id, presence: true
+
   belongs_to :student
 end
--- test/models/goal_test.rb
+++ test/models/goal_test.rb
@@ -1,7 +1,45 @@
 require 'test_helper'
 
 class GoalTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    # associate a goal first
+    @student = Student.first
+
+    @goal = Goal.create(number: 0, student_id: @student.id)
+  end
+
+  test "'goal' must be valid" do
+    assert @goal.valid?
+  end
+
+  test "number must be present" do
+    @goal.number = ""
+    assert_not @goal.valid?
+  end
+
+  test "number must be an integer" do
+    @goal.number = 0.5
+    assert_not @goal.valid?
+  end
+
+  test "number must be unique per student id" do
+    new_goal = Goal.create(number: @goal.number, student_id: @goal.student_id)
+    assert_not new_goal.valid?
+    new_goal = Goal.create(number: @goal.number, student_id: Student.last.id)
+    assert new_goal.valid?
+  end
+
+  test "number must be great than or equal to 0" do
+    @goal.number = -1
+    assert_not @goal.valid?
+    @goal.number = 0
+    assert @goal.valid?
+  end
+
+  test "number must be less than or equal to 3" do
+    @goal.number = 3
+    assert @goal.valid?
+    @goal.number = 4
+    assert_not @goal.valid?
+  end
 end
